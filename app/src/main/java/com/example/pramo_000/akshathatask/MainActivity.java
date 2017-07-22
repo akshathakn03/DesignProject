85e0459f5cf076b51dcfcf0f03e7bfa09ade48ba
Pramod <pamod1990@gmail.com>
2017-07-21 22:54:30 -0700

    akshatha

---
+++ app/src/main/java/com/example/pramo_000/akshathatask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pramo_000.akshathatask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
